628308ced3f1b917511ba5afa6b7dbf1e87aa974
eujin Lee <eireh001@naver.com>
2023-01-26 18:30:46 +0900

    login/signup/main

--- app/src/main/java/com/example/guru_android_team30/SignupActivity.kt
+++ app/src/main/java/com/example/guru_android_team30/SignupActivity.kt
@@ -1,11 +1,24 @@
 package com.example.guru_android_team30
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class SignupActivity : AppCompatActivity() {
+
+    lateinit var sign_up : ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        sign_up = findViewById(R.id.Sign_up)
+
+        sign_up.setOnClickListener {
+            var intent = Intent(this, Signup_CompleteActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 }
